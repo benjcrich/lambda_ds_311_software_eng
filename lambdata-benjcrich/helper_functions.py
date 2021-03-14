36bdea4ec5c85ee28f88d2ee4086d9c4d53a515d
benjcrich <benjcrich@gmail.com>
2021-03-13 19:15:34 -0700

    Update helper_functions.py

--- lambdata-benjcrich/helper_functions.py
+++ lambdata-benjcrich/helper_functions.py
@@ -1,10 +1,12 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split as tsp
+
 
 def null_count(df):
     df.isna().sum()
     return
 
+
 def train_test_split(df, frac):
-    train, val = df, train_size=frac, test_size=(frac/1)
-    return
+    train, val = tsp(df, train_size=frac, test_size=(frac/1))
+    return train, val
